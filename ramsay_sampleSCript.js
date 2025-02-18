1a2dba033511ffef25f0bf72a61b88115b7b0c43
Anchith1 <129327934+Anchith1@users.noreply.github.com>
2025-02-18 09:25:50 +0530

    Initial code Ramsay
    Initial

---
+++ ramsay_sampleSCript.js
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export let options = {
+  scenarios: {
+    iteration_test: {
+      executor: 'per-vu-iterations',
+      vus: 10,
+      iterations: 5,
+      startTime: '5s',  // Make sure this is a string ('5s')
+    }
+  }
+};
+
+const baseUrl = 'https://dev-healthhub.ramsayhealth.co.uk';  // URL should be quoted
+
+export default function () {
+  let launch = http.get(baseUrl);
+
+  // Using check to validate the response
+  check(launch, {
+    'Check if this sentence exists': (r) => r.body.includes('Find an initial consultation near you'),  // Sentence check
+    'Check response code': (r) => r.status === 200  // Correct comparison for status code
+  });
+
+  sleep(1);  // Simulate a delay before the next iteration
+}
