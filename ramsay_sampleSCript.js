b54f3b59bcbe16f67e81f5bbfb479d3269413703
EAXRT <anchith1@gmail.com>
2025-02-18 15:24:02 +0530

    Added next set of url

--- ramsay_sampleSCript.js
+++ ramsay_sampleSCript.js
@@ -5,9 +5,9 @@ export let options = {
   scenarios: {
     iteration_test: {
       executor: 'per-vu-iterations',
-      vus: 10,
-      iterations: 5,
-      startTime: '5s',  // Make sure this is a string ('5s')
+      vus: 1,
+      iterations: 2,
+      startTime: '0s',  // Make sure this is a string ('5s')
     }
   }
 };
@@ -20,6 +20,12 @@ export default function () {
   // Using check to validate the response
   check(launch, {
     'Check if this sentence exists': (r) => r.body.includes('Find an initial consultation near you'),  // Sentence check
+    'Check response code': (r) => r.status === 200  // Correct comparison for status code
+  });
+
+  let downtime = http.get(baseUrl+'/MVPDowntimeData-DFD/DFDDowntimeFlagData.json')
+  check(downtime, {
+   // 'Check if this sentence exists': (r) => r.body.includes('Find an initial consultation near you'),  // Sentence check
     'Check response code': (r) => r.status === 200  // Correct comparison for status code
   });
 
